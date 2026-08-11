bb10053d49630a7d086496b0cba47cd8f6c30c2c
sohaibnakoh-cmyk <sohaibnakoh@gmail.com>
2026-08-11 14:47:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -239,3 +239,6 @@ def webhook():
 @app.route('/')
 def index():
     return "Bot is running successfully with Webhook!", 200
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
